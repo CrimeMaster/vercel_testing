9d4648a0a7c7b80975355c9b8889a01093e7b953
CrimeMaster <gtirkey300@gmail.com>
2023-12-31 20:39:05 +0530

    first commit

--- index.js
+++ index.js
@@ -1,15 +1,8 @@
 const express = require('express');
-const home = require("./routes/home");
-const bodyParser = require('body-parser')
-const mongoose = require('mongoose')
+const app = express()
 const dotenv = require('dotenv')
 dotenv.config()
-const cors = require('cors')
-const authorization = require("./middleware/authorization");
-const authRoutes = require("./routes/auth");
-const jobRoutes = require("./routes/job");
 
-const app = express()
 
 app.get('/health', (req, res) => {
     res.json({
