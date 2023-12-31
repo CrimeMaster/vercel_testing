f3ad93a1be6a20b34a353e93c7e58fc1c6c26229
CrimeMaster <gtirkey300@gmail.com>
2023-12-31 21:34:29 +0530

    first commit

--- index.js
+++ index.js
@@ -1,21 +1,7 @@
 const express = require('express');
 const app = express()
-const bodyParser = require('body-parser')
-const mongoose = require('mongoose')
 const dotenv = require('dotenv')
-const cors = require('cors')
 dotenv.config()
-const authorization = require("./middleware/authorization");
-
-
-
-app.use(cors())
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json())
-
-//Routes
-
-
 
 
 app.get('/health', (req, res) => {
@@ -24,16 +10,6 @@ app.get('/health', (req, res) => {
         message: 'server is running',
     })
 })
-app.get("/page", authorization, (req, res) => {
-    res.json({
-      status: "active",
-      message: "running",
-    });
-  });
 app.listen(process.env.port, () => {
-    mongoose
-    .connect(process.env.MONGODB_CONNECT_URI)
-    .then(() => console.log("connection succesful"))
-    .catch((error) => console.log(error))
     console.log(`Server Running Succesfully on http://localhost:${process.env.port}`)
 })
