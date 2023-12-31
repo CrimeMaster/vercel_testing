48c099f6994b153fc52244fe96dc7c84921847fa
CrimeMaster <gtirkey300@gmail.com>
2023-12-31 20:12:58 +0530

    second commit

--- index.js
+++ index.js
@@ -1,15 +1,40 @@
-const express = require('express');
-const app = express()
-const dotenv = require('dotenv')
-dotenv.config()
+const express = require("express");
+const home = require("./routes/home");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+const cors = require("cors");
+const authorization = require("./middleware/authorization");
+const authRoutes = require("./routes/auth");
+const jobRoutes = require("./routes/job");
 
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.get('/health', (req, res) => {
-    res.json({
-        status:200,
-        message: 'server is running',
-    })
-})
+//Routes
+app.use("/home", home);
+app.use("api/auth", authRoutes);
+app.use("api/job", jobRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Backend Capstone Project");
+});
+
+app.get("/health", (req, res) => {
+  res.json({
+    status: 200,
+    message: "server is running",
+  });
+});
 app.listen(process.env.port, () => {
-    console.log(`Server Running Succesfully on http://localhost:${process.env.port}`)
-})
+  mongoose
+    .connect(process.env.MONGODB_CONNECT_URI)
+    .then(() =>
+      console.log(
+        `Server Running Succesfully on http://localhost:${process.env.port}`
+      )
+    )
+    .catch((error) => console.log(error));
+});
---
+++ model/user.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const UserSchema = mongoose.Schema({
+  fullname: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  mobile: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = new mongoose.model("user", UserSchema);
