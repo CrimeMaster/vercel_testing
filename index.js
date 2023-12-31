87b929b20942efcb7f4a4296fa16fd46f5db6e5f
CrimeMaster <gtirkey300@gmail.com>
2023-12-31 21:07:41 +0530

    fifth commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const app = express()
-const cors = require('cors')
+
 dotenv.config()
 const authorization = require("./middleware/authorization");
 
